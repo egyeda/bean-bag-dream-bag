1ea0a7d09295e75cca94c9fe442522e1164c02ae
Egyed Adam <adam.egyed19@gmail.com>
2023-02-04 13:50:46 +0100

    episode 2

--- beanbag/pages/index.vue
+++ beanbag/pages/index.vue
@@ -8,6 +8,7 @@
       <v-card>
         <v-card-title class="headline">
           {{message}}
+          episode 2
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
